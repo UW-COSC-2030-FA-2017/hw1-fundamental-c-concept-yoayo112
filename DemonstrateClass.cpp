11c1e9fcfbfca8835a017b4fb6753616902ce2a7
Sky Vercauteren <svercaut@uwyo.edu>
2017-09-17 21:26:40 -0600

    Here is the code

---
+++ DemonstrateClass.cpp
@@ -0,0 +1,19 @@
+//Schuyler Vercauteren
+//Computer Science 2
+
+//Decrlare using statements.
+#include<iostream>
+//Declare Class name.
+class myClass
+{
+//Store Three Types of Data.
+public:
+private:
+protected:
+};
+//Use a driver to create instances of the class.
+int main()
+{
+	new myClass();
+	return 0;
+}
